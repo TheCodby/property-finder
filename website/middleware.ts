cd382181c118a487bab6579e224bc54e3e8d6fd4
Codby <87335804+TheCodby@users.noreply.github.com>
2023-03-17 01:44:13 +0300

    modifying .env in webste directory

--- website/middleware.ts
+++ website/middleware.ts
@@ -3,19 +3,18 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  console.log(process.env.NEXT_PUBLIC_API_LINK);
-  console.log("test");
   let res = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/user/session`, {
     headers: {
       authorization: request.cookies.get("token")?.value as any,
     },
   });
   const session = await res.json();
+  console.log(session);
   if (request.nextUrl.pathname.startsWith("/auth")) {
-    if (session.id) {
+    if (session?.id) {
       return NextResponse.redirect(new URL("/", request.url));
     }
-  } else if (!session.id && authorized.includes(request.nextUrl.pathname)) {
+  } else if (!session?.id && authorized.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 }
